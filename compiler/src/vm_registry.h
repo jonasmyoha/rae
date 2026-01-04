216a8c91338721aed3f1b4530298b838904a75c9
Jonas Myöhä <6566641+jonasmyoha@users.noreply.github.com>
2026-01-04 14:27:08 +0200

    feat: add VM native registry scaffolding

--- compiler/src/vm_registry.h
+++ compiler/src/vm_registry.h
@@ -10,10 +10,27 @@ typedef struct {
   Chunk chunk;
 } VmModule;
 
+struct VM;
+
+typedef bool (*VmNativeCallback)(struct VM* vm,
+                                 Value* out_value,
+                                 const Value* args,
+                                 size_t arg_count,
+                                 void* user_data);
+
+typedef struct {
+  char* name;
+  VmNativeCallback callback;
+  void* user_data;
+} VmNativeEntry;
+
 typedef struct {
   VmModule* modules;
   size_t count;
   size_t capacity;
+  VmNativeEntry* natives;
+  size_t native_count;
+  size_t native_capacity;
 } VmRegistry;
 
 void vm_registry_init(VmRegistry* registry);
@@ -21,5 +38,10 @@ void vm_registry_free(VmRegistry* registry);
 VmModule* vm_registry_find(VmRegistry* registry, const char* path);
 bool vm_registry_load(VmRegistry* registry, const char* path, Chunk chunk);
 bool vm_registry_reload(VmRegistry* registry, const char* path, Chunk chunk);
+bool vm_registry_register_native(VmRegistry* registry,
+                                 const char* name,
+                                 VmNativeCallback callback,
+                                 void* user_data);
+const VmNativeEntry* vm_registry_find_native(const VmRegistry* registry, const char* name);
 
 #endif /* VM_REGISTRY_H */
