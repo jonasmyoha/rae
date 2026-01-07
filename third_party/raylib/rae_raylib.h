38e48435cc65712883359ea8ac28d6cf3dda0654
Jonas Myöhä <6566641+jonasmyoha@users.noreply.github.com>
2026-01-07 18:18:39 +0200

    refactor(raylib): Use PascalCase wrapper names and centralize in lib/raylib.rae

--- third_party/raylib/rae_raylib.h
+++ third_party/raylib/rae_raylib.h
@@ -3,16 +3,16 @@
 
 #include <stdint.h>
 
-void rae_raylib_init_window(int64_t width, int64_t height, const char* title);
-int64_t rae_raylib_window_should_close(void);
-void rae_raylib_close_window(void);
-void rae_raylib_begin_drawing(void);
-void rae_raylib_end_drawing(void);
-void rae_raylib_clear_background(int64_t r, int64_t g, int64_t b, int64_t a);
-void rae_raylib_draw_rectangle(int64_t x, int64_t y, int64_t width, int64_t height, int64_t r, int64_t g, int64_t b, int64_t a);
-void rae_raylib_draw_circle(int64_t x, int64_t y, int64_t radius, int64_t r, int64_t g, int64_t b, int64_t a);
-void rae_raylib_draw_text(const char* text, int64_t x, int64_t y, int64_t fontSize, int64_t r, int64_t g, int64_t b, int64_t a);
-void rae_raylib_set_target_fps(int64_t fps);
-int64_t rae_raylib_is_key_down(int64_t key);
+void Rae_InitWindow(int64_t width, int64_t height, const char* title);
+int64_t Rae_WindowShouldClose(void);
+void Rae_CloseWindow(void);
+void Rae_BeginDrawing(void);
+void Rae_EndDrawing(void);
+void Rae_ClearBackground(int64_t r, int64_t g, int64_t b, int64_t a);
+void Rae_DrawRectangle(int64_t x, int64_t y, int64_t width, int64_t height, int64_t r, int64_t g, int64_t b, int64_t a);
+void Rae_DrawCircle(int64_t x, int64_t y, int64_t radius, int64_t r, int64_t g, int64_t b, int64_t a);
+void Rae_DrawText(const char* text, int64_t x, int64_t y, int64_t fontSize, int64_t r, int64_t g, int64_t b, int64_t a);
+void Rae_SetTargetFPS(int64_t fps);
+int64_t Rae_IsKeyDown(int64_t key);
 
 #endif
