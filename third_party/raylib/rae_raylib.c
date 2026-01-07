0177b4c84d39fe194d3a2603caef2a016195a719
Jonas Myöhä <6566641+jonasmyoha@users.noreply.github.com>
2026-01-07 18:18:39 +0200

    refactor(raylib): Use PascalCase wrapper names and centralize in lib/raylib.rae

--- third_party/raylib/rae_raylib.c
+++ third_party/raylib/rae_raylib.c
@@ -1,46 +1,46 @@
 #include "rae_raylib.h"
 #include <raylib.h>
 
-void rae_raylib_init_window(int64_t width, int64_t height, const char* title) {
+void Rae_InitWindow(int64_t width, int64_t height, const char* title) {
     InitWindow((int)width, (int)height, title);
 }
 
-int64_t rae_raylib_window_should_close(void) {
+int64_t Rae_WindowShouldClose(void) {
     return WindowShouldClose() ? 1 : 0;
 }
 
-void rae_raylib_close_window(void) {
+void Rae_CloseWindow(void) {
     CloseWindow();
 }
 
-void rae_raylib_begin_drawing(void) {
+void Rae_BeginDrawing(void) {
     BeginDrawing();
 }
 
-void rae_raylib_end_drawing(void) {
+void Rae_EndDrawing(void) {
     EndDrawing();
 }
 
-void rae_raylib_clear_background(int64_t r, int64_t g, int64_t b, int64_t a) {
+void Rae_ClearBackground(int64_t r, int64_t g, int64_t b, int64_t a) {
     ClearBackground((Color){(unsigned char)r, (unsigned char)g, (unsigned char)b, (unsigned char)a});
 }
 
-void rae_raylib_draw_rectangle(int64_t x, int64_t y, int64_t width, int64_t height, int64_t r, int64_t g, int64_t b, int64_t a) {
+void Rae_DrawRectangle(int64_t x, int64_t y, int64_t width, int64_t height, int64_t r, int64_t g, int64_t b, int64_t a) {
     DrawRectangle((int)x, (int)y, (int)width, (int)height, (Color){(unsigned char)r, (unsigned char)g, (unsigned char)b, (unsigned char)a});
 }
 
-void rae_raylib_draw_circle(int64_t x, int64_t y, int64_t radius, int64_t r, int64_t g, int64_t b, int64_t a) {
+void Rae_DrawCircle(int64_t x, int64_t y, int64_t radius, int64_t r, int64_t g, int64_t b, int64_t a) {
     DrawCircle((int)x, (int)y, (float)radius, (Color){(unsigned char)r, (unsigned char)g, (unsigned char)b, (unsigned char)a});
 }
 
-void rae_raylib_draw_text(const char* text, int64_t x, int64_t y, int64_t fontSize, int64_t r, int64_t g, int64_t b, int64_t a) {
+void Rae_DrawText(const char* text, int64_t x, int64_t y, int64_t fontSize, int64_t r, int64_t g, int64_t b, int64_t a) {
     DrawText(text, (int)x, (int)y, (int)fontSize, (Color){(unsigned char)r, (unsigned char)g, (unsigned char)b, (unsigned char)a});
 }
 
-void rae_raylib_set_target_fps(int64_t fps) {
+void Rae_SetTargetFPS(int64_t fps) {
     SetTargetFPS((int)fps);
 }
 
-int64_t rae_raylib_is_key_down(int64_t key) {
+int64_t Rae_IsKeyDown(int64_t key) {
     return IsKeyDown((int)key) ? 1 : 0;
 }
