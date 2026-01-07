7554c29c3584e782601a918782f7cc96ac100104
Jonas Myöhä <6566641+jonasmyoha@users.noreply.github.com>
2026-01-07 19:43:54 +0200

    feat(parser): Support unquoted imports and standard library resolution

--- third_party/raylib/rae_raylib.c
+++ third_party/raylib/rae_raylib.c
@@ -1,46 +1,46 @@
 #include "rae_raylib.h"
 #include <raylib.h>
 
-void Rae_InitWindow(int64_t width, int64_t height, const char* title) {
+void initWindow(int64_t width, int64_t height, const char* title) {
     InitWindow((int)width, (int)height, title);
 }
 
-int64_t Rae_WindowShouldClose(void) {
+int64_t windowShouldClose(void) {
     return WindowShouldClose() ? 1 : 0;
 }
 
-void Rae_CloseWindow(void) {
+void closeWindow(void) {
     CloseWindow();
 }
 
-void Rae_BeginDrawing(void) {
+void beginDrawing(void) {
     BeginDrawing();
 }
 
-void Rae_EndDrawing(void) {
+void endDrawing(void) {
     EndDrawing();
 }
 
-void Rae_ClearBackground(int64_t r, int64_t g, int64_t b, int64_t a) {
+void clearBackground(int64_t r, int64_t g, int64_t b, int64_t a) {
     ClearBackground((Color){(unsigned char)r, (unsigned char)g, (unsigned char)b, (unsigned char)a});
 }
 
-void Rae_DrawRectangle(int64_t x, int64_t y, int64_t width, int64_t height, int64_t r, int64_t g, int64_t b, int64_t a) {
+void drawRectangle(int64_t x, int64_t y, int64_t width, int64_t height, int64_t r, int64_t g, int64_t b, int64_t a) {
     DrawRectangle((int)x, (int)y, (int)width, (int)height, (Color){(unsigned char)r, (unsigned char)g, (unsigned char)b, (unsigned char)a});
 }
 
-void Rae_DrawCircle(int64_t x, int64_t y, int64_t radius, int64_t r, int64_t g, int64_t b, int64_t a) {
+void drawCircle(int64_t x, int64_t y, int64_t radius, int64_t r, int64_t g, int64_t b, int64_t a) {
     DrawCircle((int)x, (int)y, (float)radius, (Color){(unsigned char)r, (unsigned char)g, (unsigned char)b, (unsigned char)a});
 }
 
-void Rae_DrawText(const char* text, int64_t x, int64_t y, int64_t fontSize, int64_t r, int64_t g, int64_t b, int64_t a) {
+void drawText(const char* text, int64_t x, int64_t y, int64_t fontSize, int64_t r, int64_t g, int64_t b, int64_t a) {
     DrawText(text, (int)x, (int)y, (int)fontSize, (Color){(unsigned char)r, (unsigned char)g, (unsigned char)b, (unsigned char)a});
 }
 
-void Rae_SetTargetFPS(int64_t fps) {
+void setTargetFPS(int64_t fps) {
     SetTargetFPS((int)fps);
 }
 
-int64_t Rae_IsKeyDown(int64_t key) {
+int64_t isKeyDown(int64_t key) {
     return IsKeyDown((int)key) ? 1 : 0;
 }
--- third_party/raylib/rae_raylib.h
+++ third_party/raylib/rae_raylib.h
@@ -3,16 +3,16 @@
 
 #include <stdint.h>
 
-void Rae_InitWindow(int64_t width, int64_t height, const char* title);
-int64_t Rae_WindowShouldClose(void);
-void Rae_CloseWindow(void);
-void Rae_BeginDrawing(void);
-void Rae_EndDrawing(void);
-void Rae_ClearBackground(int64_t r, int64_t g, int64_t b, int64_t a);
-void Rae_DrawRectangle(int64_t x, int64_t y, int64_t width, int64_t height, int64_t r, int64_t g, int64_t b, int64_t a);
-void Rae_DrawCircle(int64_t x, int64_t y, int64_t radius, int64_t r, int64_t g, int64_t b, int64_t a);
-void Rae_DrawText(const char* text, int64_t x, int64_t y, int64_t fontSize, int64_t r, int64_t g, int64_t b, int64_t a);
-void Rae_SetTargetFPS(int64_t fps);
-int64_t Rae_IsKeyDown(int64_t key);
+void initWindow(int64_t width, int64_t height, const char* title);
+int64_t windowShouldClose(void);
+void closeWindow(void);
+void beginDrawing(void);
+void endDrawing(void);
+void clearBackground(int64_t r, int64_t g, int64_t b, int64_t a);
+void drawRectangle(int64_t x, int64_t y, int64_t width, int64_t height, int64_t r, int64_t g, int64_t b, int64_t a);
+void drawCircle(int64_t x, int64_t y, int64_t radius, int64_t r, int64_t g, int64_t b, int64_t a);
+void drawText(const char* text, int64_t x, int64_t y, int64_t fontSize, int64_t r, int64_t g, int64_t b, int64_t a);
+void setTargetFPS(int64_t fps);
+int64_t isKeyDown(int64_t key);
 
 #endif
