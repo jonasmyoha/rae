952b0541230dab69c5597f059ed4ba782f745725
Jonas Myöhä <6566641+jonasmyoha@users.noreply.github.com>
2026-01-08 18:40:45 +0200

    fix(examples/advanced_pong): Improve collision detection and cleanup VM

--- third_party/raylib/rae_raylib.h
+++ third_party/raylib/rae_raylib.h
@@ -9,9 +9,9 @@ void closeWindow(void);
 void beginDrawing(void);
 void endDrawing(void);
 void clearBackground(int64_t r, int64_t g, int64_t b, int64_t a);
-void drawRectangle(int64_t x, int64_t y, int64_t width, int64_t height, int64_t r, int64_t g, int64_t b, int64_t a);
-void drawCircle(int64_t x, int64_t y, int64_t radius, int64_t r, int64_t g, int64_t b, int64_t a);
-void drawText(const char* text, int64_t x, int64_t y, int64_t fontSize, int64_t r, int64_t g, int64_t b, int64_t a);
+void drawRectangle(double x, double y, double width, double height, int64_t r, int64_t g, int64_t b, int64_t a);
+void drawCircle(double x, double y, double radius, int64_t r, int64_t g, int64_t b, int64_t a);
+void drawText(const char* text, double x, double y, double fontSize, int64_t r, int64_t g, int64_t b, int64_t a);
 void setTargetFPS(int64_t fps);
 int64_t isKeyDown(int64_t key);
 
