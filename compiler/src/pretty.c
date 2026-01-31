072f4004f27ba2014baaae4e886f963366ed6fb1
Jonas Myöhä <6566641+jonasmyoha@users.noreply.github.com>
2026-01-31 05:14:39 +0200

    Rae v0.3 Final: Enforce typed ret literals, fix formatter indentation, and update all libraries/examples

--- compiler/src/pretty.c
+++ compiler/src/pretty.c
@@ -437,6 +437,7 @@ static void pp_expr_prec(PrettyPrinter* pp, const AstExpr* expr, int parent_prec
             field = field->next;
           }
           pp->indent--;
+          pp_newline(pp);
         } else {
           pp_space(pp);
           AstObjectField* field = expr->as.object_literal.fields;
@@ -574,6 +575,7 @@ static void pp_expr_prec(PrettyPrinter* pp, const AstExpr* expr, int parent_prec
             current = current->next;
           }
           pp->indent--;
+          pp_newline(pp);
         } else {
           pp_space(pp);
           int first = 1;
@@ -612,6 +614,7 @@ static void pp_expr_prec(PrettyPrinter* pp, const AstExpr* expr, int parent_prec
             current = current->next;
           }
           pp->indent--;
+          pp_newline(pp);
         } else {
           int first = 1;
           while (current) {
@@ -916,6 +919,7 @@ static void pp_print_params(PrettyPrinter* pp, const AstParam* param) {
       current = current->next;
     }
     pp->indent--;
+    pp_newline(pp);
   } else {
     int first = 1;
     while (current) {
@@ -957,6 +961,7 @@ static void pp_print_return_items(PrettyPrinter* pp, const AstReturnItem* item) 
       current = current->next;
     }
     pp->indent--;
+    pp_newline(pp);
   } else {
     int first = 1;
     while (current) {
