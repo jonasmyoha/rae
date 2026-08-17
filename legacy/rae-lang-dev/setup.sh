c2dba33832deb76d0b7979e9ac551d18356d20fa
Jonas Myöhä <6566641+jonasmyoha@users.noreply.github.com>
2026-08-17 14:41:18 +0300

    Point submodule + clone URLs at new GitHub username (jonaskivi -> jonasmyoha)

--- legacy/rae-lang-dev/setup.sh
+++ legacy/rae-lang-dev/setup.sh
@@ -6,11 +6,11 @@ ROOT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$ROOT_DIR"
 
 if [ ! -d rae ]; then
-  git clone git@github.com:jonaskivi/rae.git
+  git clone git@github.com:jonasmyoha/rae.git
 fi
 
 if [ ! -d rae-devtools-web ]; then
-  git clone git@github.com:jonaskivi/rae-devtools-web.git
+  git clone git@github.com:jonasmyoha/rae-devtools-web.git
 fi
 
 echo "Rae workspace ready:"
